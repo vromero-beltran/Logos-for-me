f7ca6a1355209d9307288bd931cde1c22b0b3479
Victor <frozen711.vr.vr@gmail.com>
2023-10-04 21:30:31 -0400

    tutor helped fix problems

--- index.js
+++ index.js
@@ -1,97 +1,105 @@
-const fs = require('fs');
-const inquirer = require('inquirer'); 
-const { Circle, Square, Triangle } = require('./shapes');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./shapes");
 
 class Svg {
-    constructor() {
-        this.textElement = ''
-        this.shapeElement = ''
-    };
+  constructor() {
+    this.textElement = "";
+    this.shapeElement = "";
+  }
 
-    render (){
-        return `<svg version = "1.1" xmlns = "http://www.w3.org/2000/svg" width = "300"
-        height = "200"> ${this.shapeElement}${this.textElement}</svg>`
-    };
+  render() {
+    return `<svg version = "1.1" xmlns = "http://www.w3.org/2000/svg" width = "300"
+        height = "200"> 
+        ${this.shapeElement}
+        ${this.textElement}
+        </svg>`;
+  }
 
-    setTextElement(text, color) {
-        this.textElement = `<text x="150" y="100" fill="${color}" text-anchor="middle">${text}</text>`
-    };
+  setTextElement(text, color) {
+    this.textElement = `<text x="150" y="100" fill="${color}" text-anchor="middle">${text}</text>`;
+  }
 
-    setShapeElement(shape) {
-        this.shapeElement = shape.render()
-    };
-};
+  setShapeElement(Shape) {
+    this.shapeElement = Shape.render();
+    
+  }
+}
 
 const questions = [
-    {
-        type: "input",
-        name: 'text',
-        message:"TEXT: Enter up to (3) Characters",
-    },
-    {
-        type: "input",
-        name: "textColor",
-        message:'COLOR TEXT: Enter a color keyword (OR a hexadecimal number:)',
-    },
-    {
-        type: "input",
-        name: "shapeColor",
-        message:"SHAPE COLOR: Enter a color keyword (OR a hexadecimal number:)",
-    },
-    {
-        type: "list",
-        name: "pixalImage",
-        message: "Select a shape:",
-        choices: ["Circle", "Square", "Triangle"],
-    },
+  {
+    type: "input",
+    name: "text",
+    message: "TEXT: Enter up to (3) Characters",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "COLOR TEXT: Enter a color keyword (OR a hexadecimal number:)",
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number:)",
+  },
+  {
+    type: "list",
+    name: "Shape",
+    message: "Select a shape:",
+    choices: ["Circle", "Square", "Triangle"],
+  },
 ];
 
-function generateLogo() {
-    // Prompt user for input
-    const answers = questions;
-    // Get values from answers
-    const { text, textColor, Shape, shapeColor } = answers;
-    // Create SVG instance
-    const svg = new Svg();
-    // Set text and color
-    svg.setTextElement(text, textColor);
-    // Create shape instance
-    let shapeElement;
-    if (Shape === 'Circle') {
-        shapeElement = new Circle();
-    } else if (Shape === 'Square') {
-        shapeElement = new Square();
-    } else {
-        shapeElement = new Triangle();
-    }
-    // Set shape color
-    shapeElement.setColor(shapeColor);
-    // Set shape on SVG
-    svg.setShapeElement(shapeElement);
-    // Render SVG code
-    const svgCode = svg.render();
-    // Write SVG file
-    fs.writeFileSync('logo.svg', svgCode);
-    // Log message
-    console.log('Generated logo.svg'); 
+function generateLogo(data) {
+  // Get values from answers
+  const { text, textColor, Shape, shapeColor } = data;
+  // Create SVG instance
+  const svg = new Svg();
+  // Set text and color
+  //console.log(text, textColor);
+  svg.setTextElement(text, textColor);
+  //svg.showtextElement();
+
+  // Create shape instance
+  let shapeElement;
+  if (Shape === 'Circle') {
+      shapeElement = new Circle();
+  } else if (Shape === 'Square') {
+      shapeElement = new Square();
+  } else {
+      shapeElement = new Triangle();
+  }
+
+  // Set shape color
+  shapeElement.setColor(shapeColor);
+
+  // Set shape on SVG
+  svg.setShapeElement(shapeElement);
+
+
+  // Render SVG code
+  const svgCode = svg.render();
+  // Write SVG file
+  fs.writeFileSync('logo.svg', svgCode);
+   // Log message
+   console.log('Generated logo.svg');
 }
 
 function writeToFile(fileName, data) {
-    var content = generateLogo(data);
-    fs.writeFile(fileName, content, function(error) {
-        if (error){
-            return console.log(error);
-        }
-        console.log("Generated logo.svg");
-    });
+  var content = generateLogo(data);
+//   fs.writeFile(fileName, content, function (error) {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     console.log("Generated logo.svg");
+//   });
 }
 
 function init() {
-    inquirer.prompt(questions).then(function (data) {
-        var fileName = 'logo.svg';
-        writeToFile(fileName, data);
-    })
+  inquirer.prompt(questions).then(function (data) {
+    var fileName = "logo.svg";
+    writeToFile(fileName, data);
+  });
 }
 
 init();
-
--- shape.test.js
+++ shape.test.js
@@ -5,9 +5,11 @@ describe('Circle', () => {
         const shape = new Circle();
         var color = ('blue');
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<circle cx = "50%" 
-        cy = "50%" r = "100" height = "100%" width = "100%" 
-        fill = "${color}" />`);
+        expect(shape.render()).toEqual(`<circle
+        cx="50" 
+        cy="50" 
+        r="100" 
+        fill="${color}"/>`);
     });
 });
 
@@ -16,8 +18,11 @@ describe('Square', () => {
         const shape = new Square();
         var color = ('red');
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<rect x = "50" height = "200" 
-        width = "200" fill = "${color}" />`);
+        expect(shape.render()).toEqual(`<rect
+        x="50" 
+        height="200" 
+        width="200" 
+        fill="${color}"/>`);
     });
 });
 
@@ -26,7 +31,10 @@ describe('Triangle', () => {
         const shape = new Triangle();
         var color = ('green');
         shape.setColor(color);
-        expect(shape.render()).toEqual(`<polygon height = "100%" width = "100%"
-        points="0,200 300,200 150,0" fill="${color}" />`);
+        expect(shape.render()).toEqual(`<polygon
+        height="100%" 
+        width="100%"
+        points="0,200 300,200 150,0" 
+        fill="${color}"/>`);
     });
 });
